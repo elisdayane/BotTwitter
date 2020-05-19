cc28f57cc4db5ead3b5a9d7c3647f7f4015c1905
Elis Souza <eldayane@gmail.com>
2020-05-18 22:15:34 -0300

    RespotaDTO

---
+++ TwitterBot/DTO/RepostaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitterBot.DTO
+{
+    public class RepostaDTO
+    {
+        public int Id { get; set; }
+        public string Tag { get; set; }
+        public string NomeUsuario { get; set; }
+        public int IdUsuario { get; set; }
+        public string Texto { get; set; }
+        public long NumeroSefuidores { get; set; }
+        public DateTime DataPostagem { get; set; }
+
+    }
+}
